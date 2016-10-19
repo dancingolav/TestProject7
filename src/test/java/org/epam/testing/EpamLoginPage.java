30db8a4a4e8e6b2f71302138dd6ded9a06b5804e
AlexSh <nivaland@yandex.ru>
2016-10-19 08:54:21 +0400

    1-st with login test only

---
+++ src/test/java/org/epam/testing/EpamLoginPage.java
@@ -0,0 +1,101 @@
+package org.epam.testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by AlexSh on 18.10.2016.
+ */
+public class EpamLoginPage {
+
+    //LoginLogout Menu Open
+    private By menuOpen = By.xpath("//li[@class='dropdown uui-profile-menu open']");
+    //LoginLogout Menu Closed
+    private By menuClosed = By.xpath("//li[@class='dropdown uui-profile-menu']");
+
+    //Login Menu
+    private By inputAccountName = By.xpath("//input[@id='Login']");
+    private By inputAccountPwd = By.xpath("//input[@id='Password']");
+    private By enterButton = By.xpath("//button[@class='uui-button dark-blue btn-login']/span[text()='Enter']");
+
+    //Logout Menu
+    private By logoutButton = By.xpath("//div[@class='logout']/button[@class='uui-button dark-blue btn-login'][@type='submit']/span[text()='Logout']");
+
+    //Login is failed
+    private By loginFailed = By.xpath("//span[@class='login-txt'][text()='* Login Faild']");
+
+    //Login is succeed
+
+    //We will use logoutButton to check
+    //Also we can check access to resources that successful login gives us but not now :)
+
+    private By buttonToOpenOrCloseMenu = By.xpath("//a[@class='dropdown-toggle'][@href='#']");
+
+    private String epamLoginPageUrl = "https://jdi-framework.github.io/tests/";
+
+
+
+
+    private WebDriver driverHere;
+
+    public EpamLoginPage( final WebDriver driver ) {
+        driverHere = driver;
+
+    }
+
+public void open() {
+
+    driverHere.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    driverHere.get(epamLoginPageUrl);
+    driverHere.manage().window().maximize();
+
+}
+
+
+public void login(final String accountName, final String accountPwd) {
+
+    driverHere.findElement(inputAccountName).click();
+    driverHere.findElement(inputAccountName).clear();
+    driverHere.findElement(inputAccountName).sendKeys(accountName);
+
+    driverHere.findElement(inputAccountPwd).click();
+    driverHere.findElement(inputAccountPwd).clear();
+    driverHere.findElement(inputAccountPwd).sendKeys(accountPwd);
+
+    driverHere.findElement(enterButton).click();
+}
+
+public boolean isLoginOrLogoutMenuOpen() {
+    return driverHere.findElements(menuOpen).size() !=0 ;
+}
+
+public boolean isLoginOrLogoutMenuClosed() {
+        return driverHere.findElements(menuClosed).size() !=0 ;
+}
+
+public void  openLoginOrLogoutMenu() {
+        driverHere.findElement(buttonToOpenOrCloseMenu).click();
+}
+
+public void logout() {
+        driverHere.findElement(logoutButton).click();
+}
+
+public boolean isLogoutMenuOpen() {
+    return driverHere.findElements(menuOpen).size() !=0
+            && driverHere.findElements(logoutButton).size()!=0;
+
+}
+
+public boolean isLoginFailed (){
+    return (driverHere.findElements(loginFailed)).size() != 0;
+}
+
+public boolean isLoginSucceed(){
+    return (driverHere.findElements(logoutButton)).size() != 0;
+}
+
+}
+
---
+++ src/test/java/org/epam/testing/EpamTest31v.java
@@ -0,0 +1,131 @@
+package org.epam.testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by AlexSh on 18.10.2016.
+ */
+public class EpamTest31v {
+
+
+    private WebDriver myPersonalDriver;
+    private EpamLoginPage epamLoginPage;
+
+
+    /*
+    in testng.xml
+    f.e. browser: "firefox","chrome","ie", "opera"
+    f.e. pathToDriver: "D:\\PersonalDrivers\\geckodriver.exe",
+     */
+
+    @BeforeSuite
+    @Parameters({"browser", "pathToDriver"})
+    public void beforeSuite(@Optional("chrome") String browser, @Optional("D:\\PersonalDrivers\\chromedriver.exe") String pathToDriver) {
+
+        ArrayList<String> browsersHerd = new ArrayList<String>(Arrays.asList(new String[]{"firefox", "chrome", "ie", "opera"}));
+
+        //System's properties we have to set to use drivers
+        String[] sysProperty = new String[]{
+                "webdriver.gecko.driver",
+                "webdriver.opera.driver",
+                "webdriver.ie.driver",
+                "webdriver.chrome.driver"
+        };
+        //Checking whether file is exist
+        File f = new File(pathToDriver);
+        if (!f.exists() || f.isDirectory()) {
+            System.out.println("Error! Check your browser's path in testng.xml!");
+            Assert.fail("Error! Check your browser's path in testng.xml!");
+        }
+        //Checking  whether browser of correct type
+        if (!browsersHerd.contains(browser)) {
+            System.out.println("Error! Check your browser type in testng.xml!");
+            System.out.println("firefox,chrome,ie,opera");
+            Assert.fail("Error! Check your browser type testng.xml!");
+        }
+
+
+        //Since Java 7 we can use String
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                System.setProperty(sysProperty[0], pathToDriver);
+                myPersonalDriver = new FirefoxDriver();
+                break;
+            case "opera":
+                System.setProperty(sysProperty[1], pathToDriver);
+                myPersonalDriver = new OperaDriver();
+                break;
+            case "ie":
+                System.setProperty(sysProperty[2], pathToDriver);
+                myPersonalDriver = new InternetExplorerDriver();
+                break;
+            case "chrome":
+                System.setProperty(sysProperty[3], pathToDriver);
+                myPersonalDriver = new ChromeDriver();
+                break;
+        }
+
+
+        System.out.println(browser + " " + pathToDriver);
+        epamLoginPage = new EpamLoginPage(myPersonalDriver);
+
+        epamLoginPage.open();
+
+    }
+
+    @Test(dataProviderClass=DataForLogin.class, dataProvider="dataforlogin")
+
+    public void tryLogin(boolean testType, String accountName, String accountPwd) {
+        System.out.println("logging into the account");
+
+
+
+        //if login-logout menu is closed we will open it
+        if (!(epamLoginPage.isLoginOrLogoutMenuOpen())) {
+              epamLoginPage.openLoginOrLogoutMenu();
+        }
+
+
+        //if we've opened logout menu (look at previous code) we have to log out since we were in "logged in" state
+        //and have not input fields for our data
+        if  (epamLoginPage.isLogoutMenuOpen()) {
+              epamLoginPage.logout();
+         }
+
+        epamLoginPage.login(accountName,accountPwd);
+
+
+        if (! testType)
+            //Is login failed? It has to be...
+            assertTrue(epamLoginPage.isLoginFailed());
+        else {
+            //Is login succeed? It has to be...
+            assertTrue(epamLoginPage.isLoginSucceed());
+        }
+    }
+
+
+    @AfterSuite
+    public void afterSuite() {
+        //Close the driver
+        myPersonalDriver.close();
+        myPersonalDriver.quit();
+
+    }
+
+
+}
